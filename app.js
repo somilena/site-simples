62a7d86e54e8a7d95e4c9add36abae972af48f57
Milena Souza <61788510+somilena@users.noreply.github.com>
2022-03-21 16:54:32 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+// Comportamentos e interatividade //
+
+'use strict'
+
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme')
+
+    var className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "Dark";
+    }
+    else {
+        this.textContent = "Light";
+    }
+
+    console.log('current class name: ' + className);
+});
